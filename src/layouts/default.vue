49cb1646f54969b613c0b2219333224916e2bce3
Tacxticx88 <12997062+tacxticx88@users.noreply.github.com>
2021-04-24 15:34:42 +0200

    first commit

---
+++ src/layouts/default.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  v-app(:style="appStyle")
+    v-main.h-screen
+      v-app-bar(dense dark color="green darken-2")
+        v-app-bar-title TacxTV
+      v-container(fluid :style="{height: 'calc(100% - 48px)'}")
+        nuxt
+</template>
+
+<script>
+// noinspection JSUnusedGlobalSymbols
+export default {
+  computed: {
+    appStyle() {
+      let out = {}
+      let currentTheme = this.$vuetify.theme.currentTheme
+      for (let index in currentTheme) {
+        if (currentTheme.hasOwnProperty(index)) {
+          out[`--v-color-${index}`] = currentTheme[index]
+        }
+      }
+      return out
+    },
+  },
+}
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  twitch-player(
+    channel="TacxTV"
+  )
+</template>
+
+<script>
+
+// noinspection JSUnusedGlobalSymbols
+export default {
+  name: 'IndexPage',
+}
+</script>
