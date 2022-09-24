879e85c9eb949fbcfe1d68d0b3151eb6f7335d75
Tacx <12997062+tacxticx88@users.noreply.github.com>
2022-09-24 02:00:55 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,10 +6,10 @@ export default {
 
   telemetry: false,
 
-  server: {
-    port: 3000, // default: 3000,
-    host: '0.0.0.0',
-  },
+  // server: {
+  //   port: 3000, // default: 3000,
+  //   host: '0.0.0.0',
+  // },
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
