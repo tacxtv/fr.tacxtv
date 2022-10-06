b6a10e6f1ffca8c536185cb88168bf916e8f548b
Tacxticx88 <12997062+tacxticx88@users.noreply.github.com>
2022-10-06 17:36:22 +0200

    Create team.vue

---
+++ src/pages/team.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  span Accueil
+</template>
+
+<script>
+
+// noinspection JSUnusedGlobalSymbols
+export default {
+  name: 'IndexPage',
+  middleware: (context) => {
+    context.redirect('/stream')
+  },
+}
+</script>
