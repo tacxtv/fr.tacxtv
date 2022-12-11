44b347f0e4aa7a0dc72d72983eecb5e0077d0aa7
Tacx <12997062+tacxticx88@users.noreply.github.com>
2022-12-11 19:17:32 +0100

    Update planning.vue

--- src/pages/planning.vue
+++ src/pages/planning.vue
@@ -3,7 +3,7 @@
     iframe(
       style="border: 1px solid rgba(0, 0, 0, 0.1);"
       width="100%" height="100%"
-      src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FHwJWx5iK6fZ8rG3TNvnVGw%2FPlanning%3Fnode-id%3D350%253A50%26scaling%3Dcontain%26page-id%3D315%253A2&hide-ui=1"
+      src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FHwJWx5iK6fZ8rG3TNvnVGw%2FPlanning%3Fnode-id%3D979%253A27%26scaling%3Dcontain%26page-id%3D315%253A2&hide-ui=1"
       allowfullscreen
     )
 </template>
