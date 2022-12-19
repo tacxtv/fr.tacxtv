b84a728ba0fe5a7b03f54cd411de63a480215ef0
Tacx <12997062+tacxticx88@users.noreply.github.com>
2022-12-19 02:52:17 +0100

    fix

--- src/pages/experiences.vue
+++ src/pages/experiences.vue
@@ -8,17 +8,25 @@
       .py-4
         h2.text-center(:class="`headline font-weight-light mb-4 ${experience.color}--text`" v-text="experience.title")
         v-card.mx-auto
-          v-tabs(:color="experience.color" vertical)
+          v-tabs(:color="experience.color" :value="tabs[i]" vertical)
             v-tab(style="min-width: initial")
               v-icon mdi-text
-            //v-tab(style="min-width: initial")
-            //  v-icon mdi-youtube
+            v-tab(v-if="experience.youtube" style="min-width: initial")
+              v-icon mdi-youtube
             //v-tab(style="min-width: initial")
             //  v-icon mdi-circle
             v-tab-item
               v-card-text(v-text="experience.text")
-            //v-tab-item
-            //  iframe(width="100%" height="315" src="https://www.youtube.com/embed/Jh_-RgkEp2E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+              v-card-actions
+                v-spacer
+                v-btn(icon @click="stepTab(i)")
+                  v-icon mdi-arrow-down
+            v-tab-item(v-if="experience.youtube")
+              iframe(width="100%" height="400" :src="experience.youtube.iframe.src" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+              v-card-actions
+                v-spacer
+                v-btn(icon @click="stepTab(1)")
+                  v-icon mdi-arrow-down
           v-card-actions.pa-1
             v-spacer
             //v-btn(color="primary" href="" target="_blank" text)
@@ -32,13 +40,33 @@
 // noinspection JSUnusedGlobalSymbols
 export default {
   name: 'IndexPage',
+  layout: 'viewport',
   computed: {
     getExperiences() {
       return this.experiences.reverse()
     }
   },
+  methods: {
+    stepTab(i) {
+      this.tabs[i]++
+      debugger
+    },
+  },
   data: () => ({
+    tabs: [],
     experiences: [
+      {
+        color: 'green',
+        year: 'Avril/Mai 2021',
+        title: 'Les débuts',
+        icon: 'mdi-controller',
+        text: 'On se demande comment cétait !',
+        youtube: {
+          iframe: {
+            src: 'https://www.youtube.com/embed/g0DL4-S-pLQ',
+          },
+        },
+      },
       {
         color: 'green',
         year: 'Avril/Mai 2021',
@@ -52,6 +80,11 @@ export default {
         title: 'Stream des 6 mois en retard',
         icon: 'mdi-cake-variant-outline',
         text: 'Stream des 6 mois en retard - Au menu, cosplay de Meg Thomas (DBD), des giveaways et de la folie furieuse pendant 12h ! Frémissant et intense, il est le premier gros événement de la chaine !',
+        youtube: {
+          iframe: {
+            src: 'https://www.youtube.com/embed/WQcWVnR76gI',
+          },
+        },
       },
       {
         color: 'orange',
@@ -66,11 +99,16 @@ export default {
         title: 'Caritatif event - Arma des dons',
         icon: 'mdi-hand-coin',
         text: 'Evenement caritatif au profit de La ligue contre le cancer. Tout en distanciel, cet évèment aussi intensif qu\'explosif à durer 24h, a rapporter 83240€ à la ligue avec une contribution de notre part de 2622.22€. Notre commu a réellement du talent',
+        youtube: {
+          iframe: {
+            src: 'https://www.youtube.com/embed/Jh_-RgkEp2E',
+          },
+        },
       },
       {
         color: 'red',
         year: '12/2021',
-        title: 'Calendrier de l\'avant de TacxTV',
+        title: 'Calendrier de l\'avant de TacxTV 2021',
         icon: 'mdi-candycane',
         text: 'Evenement de Noel, plein de surprise et de cadeau pour le bonheur des petits et des grands pendant tout le mois de décembre',
       },
@@ -94,6 +132,11 @@ export default {
         title: 'Anniversaire des 1 an',
         icon: 'mdi-cake-variant-outline',
         text: 'Les 1 an de stream, 12h de live en folie avec plein de surprise, de jeu, les amis et la communauté au taquet',
+        youtube: {
+          iframe: {
+            src: 'https://www.youtube.com/embed/DNnihiXhe-M',
+          },
+        },
       },
       {
         color: 'blue',
@@ -115,6 +158,11 @@ export default {
         title: 'Caritatif event - USPLE',
         icon: 'mdi-hand-coin',
         text: 'Une semaine pour l\'espoir disait t\'il ! Un événement caritatif au profit de Care France qui s\'est déroulé à Paris. 4 jours distanciel et 3 jours présentiel où nous avons contribué de 2023€ à la cagnote et qui a rapporter 71193€ au total à Care France. Comme quoi dans la vie il y a de l\'espoir.',
+        youtube: {
+          iframe: {
+            src: 'https://www.youtube.com/embed/8OQG40XqLgM',
+          },
+        },
       },
       {
         color: 'blue',
@@ -145,20 +193,42 @@ export default {
         text: 'TKT c\'est dans la poche, mais TMTC c\'est pas gagné',
       },
       {
-        color: 'blue',
-        year: '10/10/2022',
-        title: 'Tournoi TKT 3 Keeps',
-        icon: 'mdi-controller',
+        color: 'pink',
+        year: 'xx/11/2022',
+        title: 'Hallowish 2022',
+        icon: 'mdi-halloween',
         text: 'TKT c\'est dans la poche, mais TMTC c\'est pas gagné',
+        // youtube: {
+        //   iframe: {
+        //     src: 'https://www.youtube.com/embed/wjc-BR5YUgc',
+        //   },
+        // },
       },
       {
         color: 'purple',
         year: '11-13/11/2022',
-        title: 'Caritatif event - Téléthon Gaming partie 1',
+        title: 'Caritatif event - Téléthon Gaming (partie 1)',
         icon: 'mdi-hand-coin',
         text: 'TKT c\'est dans la poche, mais TMTC c\'est pas gagné',
+        youtube: {
+          iframe: {
+            src: 'https://www.youtube.com/embed/wjc-BR5YUgc',
+          },
+        },
+      },
+      {
+        color: 'purple',
+        year: '02-03/12/2022',
+        title: 'Caritatif event - Téléthon Digital (partie 2)',
+        icon: 'mdi-hand-coin',
+        text: 'La version télévision du Téléthon Gaming !',
       },
     ],
   }),
+  mounted() {
+    for (const key of this.experiences) {
+      this.$set(this.tabs, key, this.tabs[key]++)
+    }
+  }
 }
 </script>
